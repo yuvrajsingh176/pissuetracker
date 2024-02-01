9f3ce489b271f3d3f41018f80084d8da8af13bb2
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-02-01 17:11:26 +0530

    implemented client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,22 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod'
 import prisma from "@/prisma/client";
-const createIssuesschema=z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
-})
+import { createIssuesschema } from "../../Validschema";
 export async function POST(request: NextRequest) {
-    const body = await request.json();
-    const valid = createIssuesschema.safeParse(body);
-    if (!valid.success) {
-        return NextResponse.json(valid.error.errors,{status:400})
-    }
-    const newIssue =await prisma.issue.create({
-        data: {
-            title: body.title,
-            description:body.description
-        }
-    })
-    return NextResponse.json(newIssue,{status:201})
-     
+  const body = await request.json();
+  const valid = createIssuesschema.safeParse(body);
+  if (!valid.success) {
+    return NextResponse.json(valid.error.errors, { status: 400 });
+  }
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
@@ -7,19 +7,20 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { VscBracketError } from "react-icons/vsc";
 import { useState } from "react";
-interface IssueForm {
-  title: string;
-  description: string;
-}
-
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssuesschema } from "@/app/Validschema";
+import { z } from "zod";
+type IssueForm =  z.infer<typeof createIssuesschema>;
 const NewIssuesPage = () => {
   const [error, setError] = useState("");
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit ,formState:{errors}} = useForm<IssueForm>({
+    resolver: zodResolver(createIssuesschema),
+  });
   const router = useRouter();
   console.log(error);
   return (
     <div className="max-w-xl">
-      {error.length>0 && (
+      {error.length > 0 && (
         <Callout.Root color="red" className="mb-5">
           <Callout.Icon>
             <VscBracketError />
@@ -40,7 +41,8 @@ const NewIssuesPage = () => {
       >
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
-        </TextField.Root>
+              </TextField.Root>
+              {errors.title && <Text color="red" as="p">{ errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
@@ -48,6 +50,7 @@ const NewIssuesPage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+              {errors.description && <Text color="red" as="p">{ errors.description.message}</Text>}
 
         <Button>Submit New Issue</Button>
       </form>
