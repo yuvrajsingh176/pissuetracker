c2b9e6a719c037890aea5e00de4570b6af4eee59
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-02-07 10:53:28 +0530

    show a confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,15 +1,36 @@
+"use client";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
     <div>
-      <Button color="red" className="w-1/2 text-center cursor-pointer">
-        <MdOutlineDeleteOutline />
-        <Link href={`/issues/${issueId}/edit`}>Delete</Link>
-      </Button>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red" className="w-1/2 text-center cursor-pointer">
+            <MdOutlineDeleteOutline />
+            Delete
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure You want to Delete? This action can't be undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red">Delete</Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
     </div>
   );
 };
