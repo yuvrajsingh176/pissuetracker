daf22154cb21a9b7cd44704a386c7d2ff5985a5d
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-02-07 11:01:21 +0530

    built delete api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -25,3 +25,15 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     })
     return NextResponse.json(updatedIssue);
 }
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const issue = await prisma.issue.findUnique({
+        where:{id:parseInt(params.id)}
+    })
+    if (!issue) {
+        return NextResponse.json({ error: "Issue not found" }, { status: 400 });
+    }
+    const deletedIssue = await prisma.issue.delete({
+        where: { id: issue.id }
+    })
+    return NextResponse.json({deletedIssue})
+}
