791daca2bb8c6002235f6c014c049777ad4998fd
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-02-12 14:21:50 +0530

    changes in the layout of navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ImBug } from "react-icons/im";
 import classnames from "classnames";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 export const Navbar = () => {
   const path = usePathname();
@@ -19,31 +19,40 @@ export const Navbar = () => {
     },
   ];
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center ">
-      <Link href="/">
-        <ImBug />
-      </Link>
-      <ul className="flex space-x-6 ">
-        {links.map((l) => (
-          <li key={l.label}>
-            <Link
-                    className={classnames({
-                        'text-zinc-900 font-bold': l.href === path,
-                        'text-zinc-500': l.href !== path,
-                        'hover:text-zinc-800 transition-colors':true
-              })}
-              href={l.href}
-            >
-              {l.label}
+    <nav className=" border-b mb-5 px-5 py-3  ">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <ImBug />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-{status==='authenticated' && <Link href="/api/auth/signout">Signout</Link>}
-{status==='unauthenticated' && <Link href="/api/auth/signin">Signin</Link>}
-     
-      </Box>
+            <ul className="flex space-x-6 ">
+              {links.map((l) => (
+                <li key={l.label}>
+                  <Link
+                    className={classnames({
+                      "text-zinc-900 font-bold": l.href === path,
+                      "text-zinc-500": l.href !== path,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={l.href}
+                  >
+                    {l.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Signout</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Signin</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
