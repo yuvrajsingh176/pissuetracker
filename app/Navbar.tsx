5ed45dd2a84e4e3a26d9a5600b75889fffc4add6
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-02-12 15:05:11 +0530

    refactored the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -12,9 +12,26 @@ import {
   Text,
 } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
-export const Navbar = () => {
+
+const Navbar = () => {
+  return (
+    <nav className=" border-b mb-5 px-5 py-3  ">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <ImBug />
+            </Link>
+            <Navlinks />
+          </Flex>
+          <Authstatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+const Navlinks = () => {
   const path = usePathname();
-  const { status, data: session } = useSession();
   const links = [
     {
       label: "Dashboard",
@@ -26,58 +43,60 @@ export const Navbar = () => {
     },
   ];
   return (
-    <nav className=" border-b mb-5 px-5 py-3  ">
-      <Container>
-        <Flex justify="between">
-          <Flex align="center" gap="3">
-            <Link href="/">
-              <ImBug />
-            </Link>
-            <ul className="flex space-x-6 ">
-              {links.map((l) => (
-                <li key={l.label}>
-                  <Link
-                    className={classnames({
-                      "text-zinc-900 font-bold": l.href === path,
-                      "text-zinc-500": l.href !== path,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={l.href}
-                  >
-                    {l.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content className="cursor-pointer">
-                  <DropdownMenu.Label>
-                    <Text size="4">{session.user?.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Signout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Signin</Link>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className="flex space-x-6 ">
+      {links.map((l) => (
+        <li key={l.label}>
+          <Link
+            className={classnames({
+              "text-zinc-900 font-bold": l.href === path,
+              "text-zinc-500": l.href !== path,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={l.href}
+          >
+            {l.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
+const Authstatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated") {
+    return (
+      <Link
+        href="/api/auth/signin"
+        className="bg-black text-white px-2 py-1   rounded-md"
+      >
+        Signin
+      </Link>
+    );
+  }
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content className="cursor-pointer">
+          <DropdownMenu.Label>
+            <Text size="4">{session!.user?.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Signout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Navbar } from "./Navbar";
 import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import AuthProvider from "./auth/Provider";
+import Navbar from "./Navbar";
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
